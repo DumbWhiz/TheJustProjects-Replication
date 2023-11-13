a7b1fe446ce46d3d00021adffc3d3e7c8ad755fa
DumbWhiz <adeyeyewilson@gmail.com>
2023-11-13 14:35:57 +0100

    commit changes
    script js and close.svg added

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburgerOpen = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav");
+
+const open = document.querySelector(".menu");
+const close = document.querySelector(".close");
+
+function hamburger() {
+    menu.classList.toggle("active");
+
+
+}
