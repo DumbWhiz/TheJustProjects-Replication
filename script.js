312206cb9236f917b7910962a13efa4c17ad2527
DumbWhiz <adeyeyewilson@gmail.com>
2023-11-13 15:06:36 +0100

    commit changes
    script.js updated

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const hamburgerOpen = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav");
+const menu = document.querySelector(".menu");
 
-const open = document.querySelector(".menu");
+const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 
 function hamburger() {
     menu.classList.toggle("active");
 
+    if(open.style.display !== "none") {
+        open.style.display = "none";
+        close.style.display = "block";
+    } else {
+        open.style.display = "block";
+        close.style.display = "none";
+    }
 
 }
